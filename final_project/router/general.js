f974ca2e6f90e74664ec0739d0e438c724e8b069
finn <finn@obrienz.com>
2024-11-28 16:41:26 -0500

    Added try/catch for error handling in promises, and proper returns in case of incorrect entries. Removed Axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,4 @@
 const express = require('express');
-//const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -29,18 +28,50 @@ public_users.post("/register", (req,res) => {
 public_users.get('/',function (req, res) {
   //res.send(JSON.stringify(books,null,4)); //Original code
   let allBooks = new Promise((resolve, reject) => {
-    resolve(JSON.stringify(books,null,4))
+    try {
+        resolve(JSON.stringify(books,null,4))
+    }
+    catch(err) {
+        reject(err);
+    }
   });
-  allBooks.then((message) => {
+  allBooks.then(
+    (message) => {
     res.send(message);
-  })
+  },
+  (err) => {
+    res.send(err);
+  }
+)
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
- });
+  /*const isbn = req.params.isbn;
+  res.send(books[isbn]);*/ // Original code
+  let isbnPromise = new Promise((resolve,reject) => {
+    try {
+        const isbn = req.params.isbn;
+        const book = books[isbn];
+        if (book){
+            resolve(book);
+        } else {
+            resolve("Can't find book with ISBN: " + isbn);
+        }
+    } catch(err) {
+        reject(err);
+    }
+   })
+    isbnPromise.then(
+        (message) => {
+            res.send(message);
+        },
+        (err) => {
+            res.send(err);
+        }
+    )
+  });
+ 
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -48,13 +79,27 @@ public_users.get('/author/:author',function (req, res) {
   let filtered_books = Object.values(books).filter((book) => book.author === author);
   res.send(filtered_books);*/ // original code
   let filterPromise = new Promise((resolve,reject) => {
-    const author = req.params.author;
-    let filtered_books = Object.values(books).filter((book) => book.author === author);
-    resolve(filtered_books);
+    try {
+        const author = req.params.author;
+        let filtered_books = Object.values(books).filter((book) => book.author === author);
+        if (filtered_books.length > 0) {
+            resolve(filtered_books);
+        } else {
+            resolve("Author " + author + " not found.")
+        }
+    }
+    catch(err) {
+        reject(err);
+    }    
   })
-  filterPromise.then((message) => {
+  filterPromise.then(
+    (message) => {
     res.send(message);
-  })
+  },
+    (err) => {
+    res.send(err);
+  }
+)
 });
 
 // Get all books based on title
@@ -63,14 +108,28 @@ public_users.get('/title/:title',function (req, res) {
     let filtered_books = Object.values(books).filter((book) => book.title === title);
     res.send(filtered_books);*/ //old code
     let titlePromise = new Promise((resolve,reject) => {
-        const title = req.params.title;
-        let filtered_books = Object.values(books).filter((book) => book.title === title);
-        resolve(filtered_books);
-    })
-    titlePromise.then((message) => {
+        try {
+            const title = req.params.title;
+            let filtered_books = Object.values(books).filter((book) => book.title === title);
+            if (filtered_books.length > 0) {
+                resolve(filtered_books);
+            } else {
+                resolve("Book with title " + title + " not found.")
+            }
+        }
+        catch(err) {
+            reject(err);
+        }    
+      })
+      titlePromise.then(
+        (message) => {
         res.send(message);
-    })
-});
+      },
+        (err) => {
+        res.send(err);
+      }
+    )
+    });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
